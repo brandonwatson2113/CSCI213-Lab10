c18de6f2e4c4f43916b6db4d357d4f623703fddb
Brandon Watson <brandonwatson2113@gmail.com>
2026-05-06 14:42:29 -0400

    Initial version of CourseInfo

---
+++ CourseInfo.java
@@ -0,0 +1,25 @@
+/*Course Name: Object-Oriented Programming
+Instructor: Dr. Elouni
+Semester: Spring 2026
+Credits: 4
+Classroom: COP 204
+*/
+ 
+
+public class CourseInfo { 
+	public static void main(String[] args) { 
+		 
+
+	    
+	    System.out.print("Course Name: Object-Oriented Programming "); 
+	  
+
+	    System.out.print("Instructor: Dr. Elouni "); 
+
+        System.out.print("Credits: 4 "); 
+
+	    System.out.println("Classroom: COP 204"); 
+	    
+
+	}
+}
