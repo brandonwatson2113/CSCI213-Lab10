122617fa878a095dfc61e6b389d349bb56ccbc42
brandonwatson2113 <brandonwatson2113@gmail.com>
2026-05-07 14:22:18 -0400

    Update CourseInfo.java

--- CourseInfo.java
+++ CourseInfo.java
@@ -8,21 +8,26 @@ Classroom: COP 204
 
 public class CourseInfo { 
 	public static void main(String[] args) { 
-		 
+		// Hard-coded
+        String courseName = "Object-Oriented Programming";
+        String instructor = "Dr. Elouni";
+        int credits = 4;
+		String classroom = "COP 204" 
+        String classTime = "4 hours";
+		String numStudents = "25";
+	   
+		// print statements
+	    System.out.print("Course Name " + courseName); 
 
+	    System.out.print("Instructor: " + instructor); 
+
+        System.out.print("Credits: " + credits); 
+
+	    System.out.println("Classroom: " + classroom); 
 	    
-	    System.out.print("Course Name: Object-Oriented Programming "); 
-	  
+	    System.out.print("classTime: " + classTime); 
 
-	    System.out.print("Instructor: Dr. Elouni "); 
-
-        System.out.print("Credits: 4 "); 
-
-	    System.out.println("Classroom: COP 204"); 
-	    
-	    System.out.print("classTime: 4 hours "); 
-
-	    System.out.println("number of students: 25"); 
+	    System.out.println("number of students: " + numStudents); 
 	    
 
 	}
